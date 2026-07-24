806644ab105f188f08a1c72a9256c4d19e521646
marcossc-beep <marcos.sc@edu.sc.senai.br>
2026-07-24 14:58:33 -0300

    feat: reformulação visual estilo Cifra Club com tema claro e verde institucional ICR

---
+++ server.js
@@ -0,0 +1,21 @@
+﻿const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname)));
+
+// Rota para buscar todos os hinos
+app.get('/api/hinos', (req, res) => {
+    fs.readFile(path.join(__dirname, 'data.json'), 'utf8', (err, data) => {
+        if (err) return res.status(500).json({ error: 'Erro ao ler dados.' });
+        res.json(JSON.parse(data));
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
