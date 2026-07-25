3f52199a5afa80521f671d17dd5551dc86b76d0b
marcossc-beep <marcos.sc@edu.sc.senai.br>
2026-07-25 09:21:13 -0300

    feat: adicionar PWA (manifest.json, sw.js) e botao de instalar aplicativo no topo superior direito

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'coletanea-icr-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/manifest.json',
+  '/icr_logo.png',
+  '/icr_logo_sem_fundo.png',
+  '/icr_logo_maior.png',
+  '/api/hinos'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200) {
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return networkResponse;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
